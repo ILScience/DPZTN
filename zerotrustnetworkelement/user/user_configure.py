9accd468bc661f2aa97cb96134ceb9c9336a03d2
Jeff Yan <yanjingfu96@163.com>
2024-12-09 16:55:39 +0800

    完成socket调试

--- zerotrustnetworkelement/user/user_configure.py
+++ zerotrustnetworkelement/user/user_configure.py
@@ -1,7 +1,7 @@
-gateway_ip = '51.1.1.1'
-# gateway_ip = '192.168.99.50'
-gateway_port = 30000
+# gateway_ip = '51.1.1.1'
+gateway_ip = '192.168.99.50'
+gateway_port = 30002
 
-user_ip = '51.1.1.3'
-# user_ip = '192.168.99.50'
-user_port = 30001
+# user_ip = '51.1.1.3'
+user_ip = '192.168.99.50'
+user_port = 30003
